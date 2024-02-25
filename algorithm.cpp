bdb6191958846a1ffd233984bbb7f7ccb1617e1c
John Christian Ventura <130374378+legilimensjohn@users.noreply.github.com>
2024-02-25 16:53:12 +0800

    Update algorithm.cpp

--- algorithm.cpp
+++ algorithm.cpp
@@ -6,14 +6,24 @@
 #include <algorithm>
 using namespace std;
 
+//Maximum number of processes
 const int MAX_PROCESSES = 7;
 
+//Process structure to store information about each process
 struct CustomProcess {
-  int id;
-  int arrivalTime;
+  int id;  //process ID
+  int arrivalTime;   
   int burstTime;
   int waitingTime;
   int turnAround;
   int remainingTime;
   int priority;
-}
+};
+
+//Queue for process IDs in Round Robin scheduling
+int processQueue [MAX_PROCESSES];
+int front = 0, rear = 0;
+
+//Array to store information about each processes
+struct CustomProcess processes[MAX_PROCESSES];
+
